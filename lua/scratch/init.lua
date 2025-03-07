1d9d44d7cb44571d400ea64647e8e600a000e11f
Greg Allan <gregallandev@gmail.com>
2025-03-06 18:43:03 -0700

    Make scratch file for repo dir, falling back to default dir for non-repos

--- lua/scratch/init.lua
+++ lua/scratch/init.lua
@@ -1,10 +1,14 @@
 -- local lfs = require("lfs")
 
 local scandir = require("plenary.scandir")
+local Job = require("plenary.job")
+local Path = require("plenary.path")
 
 local M = {}
 
 local fn = vim.fn
+
+local default_dirname = "default"
 
 ---@param path string path to file or directory
 ---@return boolean
@@ -28,7 +32,7 @@ function M.window_center(input_width)
   }
 end
 
-function M.input(opts, on_confirm, win_config)
+function M.input(opts, on_confirm, on_confirm_tbl, win_config)
   local prompt = opts.prompt or "Input: "
   local default = opts.default or ""
   on_confirm = on_confirm or function() end
@@ -67,7 +71,7 @@ function M.input(opts, on_confirm, win_config)
     vim.cmd("stopinsert")
     -- need to return "" instead of nil as is done with esc or q to close so we know to use default filename
     local input_fname = lines[1] or ""
-    on_confirm(input_fname)
+    on_confirm(input_fname, on_confirm_tbl)
     vim.api.nvim_win_close(window, true)
   end, { buffer = buffer })
 
@@ -104,10 +108,13 @@ M.setup = function(partial_config)
   end
   print("scratch root: ", M.config.scratch_files_root_dir)
 
-  M.config.default_dir = vim.fs.joinpath(M.config.scratch_files_root_dir, "default")
+  M.config.default_dir = vim.fs.joinpath(M.config.scratch_files_root_dir, default_dirname)
   print("Creating default dir: ", M.config.default_dir)
   M.create_dir(M.config.default_dir)
   M.get_scratch_files()
+  local buff_dir = M.current_buffer_dirpath()
+  M.is_git_dir(buff_dir)
+  M.get_git_repo_path(buff_dir)
 end
 
 M.get_scratch_files = function()
@@ -119,40 +126,116 @@ M.get_scratch_files = function()
   return scratch_files
 end
 
-M.cwd = function()
-  local cwd = ""
-  print("cwd: ", cwd)
-  return cwd
+M.current_buffer_dirpath = function()
+  local current_buffer_path = vim.api.nvim_buf_get_name(0)
+  print("current buffer path", current_buffer_path)
+  local current_buff_dir = Path:new(current_buffer_path):parent()
+  print("current buff dir:", current_buff_dir)
+  local buff_dir = tostring(current_buff_dir)
+
+  return buff_dir
 end
 
 M.is_git_dir = function(path)
-  local is_git = false
-  print("is git_dir", path, is_git)
+  local is_git = nil
+  local git_command = "git"
+  local rev_parse = "rev-parse"
+  local is_git_work_tree_command = { rev_parse, "--is-inside-work-tree" }
+
+  Job:new({
+    command = git_command,
+    args = is_git_work_tree_command,
+    cwd = path,
+    on_exit = function(j, return_val)
+      print(return_val)
+      print(j:result())
+      local res = j:result()
+      print(vim.inspect(res))
+      is_git = res[1] == "true"
+
+      print("is git_dir", is_git)
+    end,
+  }):sync() -- or start()
+  return is_git
 end
 
-M.get_repo = function() end
+M.get_git_repo_path = function(dirpath)
+  local git_command = "git"
+  local rev_parse = "rev-parse"
+  local repo_path = nil
+  local args = { rev_parse, "--show-toplevel" }
 
-M.create_git_dir = function()
-  print("create_git_dir")
+  Job:new({
+    command = git_command,
+    args = args,
+    cwd = dirpath,
+    on_exit = function(j, return_val)
+      print(return_val)
+      print(j:result())
+      local res = j:result()
+      print(vim.inspect(res))
+      repo_path = res[1]
+      print("repo_path", repo_path)
+    end,
+  }):sync() -- or start()
+
+  print(">>>  repo_path", repo_path)
+  return repo_path
+end
+
+M.get_dirname = function(path_str)
+  print(">>> ", path_str)
+  local path = Path:new(path_str)
+  local _path_str = tostring(path)
+  -- TODO: Ensure it is a dir!!  In use it is but check as well.
+  -- TODO: Don't use hardcoded path sep
+  local path_parts = vim.split(_path_str, "/")
+  print(vim.inspect(path_parts))
+  -- TODO: Handle no pathparts e.g. just root "/" passed in
+  return path_parts[#path_parts]
+end
+
+-- Create scratch dir from scratch root if it doesn't exist.
+-- Can be either default on setup or a new repo scratch dir.
+M.create_scratch_dir = function(dirname)
+  local scratch_dirpath = vim.fs.joinpath(M.config.scratch_files_root_dir, dirname)
+  print("creating scratch dir:", scratch_dirpath)
+  M.create_dir(scratch_dirpath)
+  return scratch_dirpath
 end
 
 M.create_file = function(fname)
   print("creating ", fname)
 end
 
-M.open_new_file = function(fname)
+M.get_new_scratch_file_dirname = function()
+  local cur_buff_dirpath = M.current_buffer_dirpath()
+  local dirname = nil
+  if M.is_git_dir(cur_buff_dirpath) then
+    local repo_name = M.get_dirname(M.get_git_repo_path(tostring(cur_buff_dirpath)))
+    dirname = repo_name
+  else
+    dirname = default_dirname
+  end
+  return dirname
+end
+
+M.open_new_file = function(fname, data)
   if fname == nil then
     return
   end
 
-  -- TODO: get git repo if we are in one
+  local dirname = data.dirname
+
+  -- make dir if does not exist
+  local new_scratch_file_dirpath = M.create_scratch_dir(dirname)
 
   if fname == "" then
     local default_fname = os.date("%Y_%m_%d__%H_%M_%S")
     fname = default_fname
   end
 
-  local fpath = vim.fs.joinpath(M.config.default_dir, fname)
+  local fpath = vim.fs.joinpath(new_scratch_file_dirpath, fname)
 
   local command = {
     M.config.open_method,
@@ -166,10 +249,9 @@ M.open_new_file = function(fname)
 end
 
 M.new_scratch_file = function()
-  --vim.ui.input({}, print)
+  local dirname = M.get_new_scratch_file_dirname()
 
-  local dirname = vim.fs.dirname(M.config.default_dir)
-  M.input({ prompt = "New scratch file in " .. dirname }, M.open_new_file, {})
+  M.input({ prompt = "Create new in " .. dirname .. " scraches" }, M.open_new_file, { dirname = dirname }, {})
 
   --  if not fname then
   -- if not M.file_exists(path) then
