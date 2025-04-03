2d92f72e79812bdd5c21ffc90bb62a5682b2a849
Greg Allan <gregallandev@gmail.com>
2025-04-02 19:30:55 -0600

    Use plenary.log; cleanup

--- lua/scratch/init.lua
+++ lua/scratch/init.lua
@@ -1,5 +1,4 @@
--- local lfs = require("lfs")
-
+local logger = require("plenary.log").new({ plugin = "scratch", level = "info", use_console = "sync" })
 local scandir = require("plenary.scandir")
 local Job = require("plenary.job")
 local Path = require("plenary.path")
@@ -89,13 +88,11 @@ function M.input(opts, on_confirm, on_confirm_tbl, win_config)
 end
 
 M.create_dir = function(path)
-  print("creating dir: " .. path, "...")
   if not M.file_exists(path) then
-    print("does not exist, making dir: ", path)
     local result = fn.mkdir(path, "p")
-    print("mkdir result: ", result)
+    logger.info("mkdir result: ", result)
   else
-    print("Already exists:", path)
+    logger.info("Already exists:", path)
     -- TODO: Check if it is a dir or a file. If a file OH NO! error! and let user know.
   end
 end
@@ -106,10 +103,10 @@ M.setup = function(partial_config)
   if M.config.scratch_files_root_dir == nil or M.config.scratch_files_root_dir == "" then
     error("Must set 'scratch_files_root_dir' in config passed to setup function")
   end
-  print("scratch root: ", M.config.scratch_files_root_dir)
+  logger.info("scratch root: ", M.config.scratch_files_root_dir)
 
   M.config.default_dir = vim.fs.joinpath(M.config.scratch_files_root_dir, default_dirname)
-  print("Creating default dir: ", M.config.default_dir)
+  logger.info("Creating default dir: ", M.config.default_dir)
   M.create_dir(M.config.default_dir)
   M.get_scratch_files()
   local buff_dir = M.current_buffer_dirpath()
@@ -118,22 +115,17 @@ M.setup = function(partial_config)
 end
 
 M.get_scratch_files = function()
-  print("get scratch files")
   local scratch_files = scandir.scan_dir(M.config.scratch_files_root_dir, { hidden = true })
-  for i, file_path in ipairs(scratch_files) do
-    print(i, file_path)
-  end
+  --  for i, file_path in ipairs(scratch_files) do
+  --  logger.info(i, file_path)
+  -- end
   return scratch_files
 end
 
 M.current_buffer_dirpath = function()
   local current_buffer_path = vim.api.nvim_buf_get_name(0)
-  print("current buffer path", current_buffer_path)
   local current_buff_dir = Path:new(current_buffer_path):parent()
-  print("current buff dir:", current_buff_dir)
-  local buff_dir = tostring(current_buff_dir)
-
-  return buff_dir
+  return tostring(current_buff_dir)
 end
 
 M.is_git_dir = function(path)
@@ -147,15 +139,11 @@ M.is_git_dir = function(path)
     args = is_git_work_tree_command,
     cwd = path,
     on_exit = function(j, return_val)
-      print(return_val)
-      print(j:result())
       local res = j:result()
-      print(vim.inspect(res))
       is_git = res[1] == "true"
-
-      print("is git_dir", is_git)
     end,
   }):sync() -- or start()
+
   return is_git
 end
 
@@ -170,27 +158,20 @@ M.get_git_repo_path = function(dirpath)
     args = args,
     cwd = dirpath,
     on_exit = function(j, return_val)
-      print(return_val)
-      print(j:result())
       local res = j:result()
-      print(vim.inspect(res))
       repo_path = res[1]
-      print("repo_path", repo_path)
     end,
   }):sync() -- or start()
 
-  print(">>>  repo_path", repo_path)
   return repo_path
 end
 
 M.get_dirname = function(path_str)
-  print(">>> ", path_str)
   local path = Path:new(path_str)
   local _path_str = tostring(path)
   -- TODO: Ensure it is a dir!!  In use it is but check as well.
   -- TODO: Don't use hardcoded path sep
   local path_parts = vim.split(_path_str, "/")
-  print(vim.inspect(path_parts))
   -- TODO: Handle no pathparts e.g. just root "/" passed in
   return path_parts[#path_parts]
 end
@@ -199,13 +180,13 @@ end
 -- Can be either default on setup or a new repo scratch dir.
 M.create_scratch_dir = function(dirname)
   local scratch_dirpath = vim.fs.joinpath(M.config.scratch_files_root_dir, dirname)
-  print("creating scratch dir:", scratch_dirpath)
+  logger.info("creating scratch dir:", scratch_dirpath)
   M.create_dir(scratch_dirpath)
   return scratch_dirpath
 end
 
 M.create_file = function(fname)
-  print("creating ", fname)
+  logger.info("creating ", fname)
 end
 
 M.get_new_scratch_file_dirname = function()
@@ -242,27 +223,14 @@ M.open_new_file = function(fname, data)
     fpath,
   }
 
-  print("command: ", command)
   vim.cmd(vim.fn.join(command, " "))
 
-  print("New scratch file created at: " .. fpath)
+  logger.info("New scratch file created at: " .. fpath)
 end
 
 M.new_scratch_file = function()
   local dirname = M.get_new_scratch_file_dirname()
-
   M.input({ prompt = "Create new in " .. dirname .. " scraches" }, M.open_new_file, { dirname = dirname }, {})
-
-  --  if not fname then
-  -- if not M.file_exists(path) then
-  --print("does not exist, making dir: ", fpath)
-  --local result = fn.
-  --print("result: ", result)
-  --else
-  --  print("Already exists:", fpath)
-  -- end
-
-  -- end
 end
 
 return M
